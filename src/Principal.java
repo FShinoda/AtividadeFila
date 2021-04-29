fbecda0c470a8f4e3aa0075bccd0a41203e15fc2
FShinoda <fernanda.shinoda@hotmail.com>
2021-04-29 12:16:48 -0300

    reservar mesa done

--- src/Principal.java
+++ src/Principal.java
@@ -9,7 +9,7 @@ public class Principal {
         Fila<Reserva> listaEspera = new Fila<Reserva>();
 
         int opcao;
-        int posRes = 0, posEsp = 0;
+        int posRes = 0;
 
         do{
             try{
@@ -17,6 +17,15 @@ public class Principal {
 
                 switch(opcao){
                     case 1: // Reservar mesa
+                        if(posRes <= 5){
+                            reservarMesa(listaReserva);
+                            posRes++;
+                        } else {
+                            reservarMesa(listaEspera);
+                            showMessageDialog(null, "As reservas estão cheias, mas seu cadastro foi armazenado na lista de espera!");
+
+                        }
+
 
                     break;
                     case 2: //Pesquisar reserva
@@ -47,9 +56,6 @@ public class Principal {
         } while(opcao != 6);
     }
         
-        
-        
-    
 
     public static String mostrarMenu(){
         String menu = "";
@@ -62,5 +68,63 @@ public class Principal {
         menu += "6. Finalizar\n";
 
         return menu;
+    }
+
+    // aux
+    public static Reserva realizarPagamento(Cliente cliente){
+        String formaPagamento = "";
+        boolean aVista;
+
+        do{
+            formaPagamento = showInputDialog(null, "Qual a forma de pagamento?\n1.À Vista\n2.Parcelado");
+
+        } while(formaPagamento.equals("1") != true && formaPagamento.equals("2") != true);
+
+        if(formaPagamento.equalsIgnoreCase("1")){
+            aVista = true;
+        } else {
+            aVista = false;
+        }
+
+        Reserva reserva = new Reserva(cliente, aVista);
+        
+        return reserva;
+    }
+
+    public static void reservarMesa(Fila<Reserva> lista){
+        int tipoCliente = 0;
+        Reserva resAux = null;
+
+        do{
+            try{
+                tipoCliente = parseInt(showInputDialog(null, "1.Pessoa Jurídica\n2.Pessoa Física"));
+
+                if(tipoCliente != 1 && tipoCliente != 2){
+                    showMessageDialog(null, "Opção inválida! Por favor tente novamente");
+                }
+
+            } catch (NumberFormatException e){
+                showMessageDialog(null, "Por favor digite um número!");
+            }
+        } while(tipoCliente != 1 && tipoCliente != 2);
+
+        if(tipoCliente == 1){
+            String nome = showInputDialog(null, "Digite o nome da empresa:");
+            String cnpj = showInputDialog(null, "Digite o cnpj");
+            
+            PessoaJuridica pJur = new PessoaJuridica(nome, cnpj);
+            resAux = realizarPagamento(pJur);
+        } else {
+            String nome = showInputDialog(null, "Digite seu nome");
+            String cpf = showInputDialog(null, "Digite seu CPF");
+
+            PessoaFisica pFis = new PessoaFisica(nome, cpf);
+
+            resAux = realizarPagamento(pFis);
+        }
+
+        // Aloca a reserva na lista
+        lista.enfileirar(resAux);
+
     }
 }
